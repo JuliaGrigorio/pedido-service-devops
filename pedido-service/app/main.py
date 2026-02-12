a7e396e0127663d2934cc61d1d8df40247ef836a
Julia <162081992+JuliaGrigorio@users.noreply.github.com>
2026-02-12 01:58:37 +0000

    Adiciona PostgreSQL e Docker Compose

--- pedido-service/app/main.py
+++ pedido-service/app/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
+import psycopg2
+import os
 
-app = FastAPI(title="Pedido Service")
+app = FastAPI()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-@app.post("/pedidos")
-def criar_pedido():
-    return {
-        "mensagem": "Pedido criado com sucesso",
-        "status": "CRIADO"
-    }
+@app.get("/db-test")
+def db_test():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT 1;")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return {"db_response": result}
